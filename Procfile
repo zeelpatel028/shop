1e70ccc178c6f96594c00be36509a7e00b0a8e75
Zeel Dobariya <zeelptl028@gmail.com>
2026-03-01 14:54:32 +0530

    port

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b 0.0.0.0:10000 app:app
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -186,6 +186,5 @@ if __name__ == '__main__':
 
     # Render environment provides PORT variable
     port = int(os.environ.get("PORT", 5000))
-    # Enable debug mode for auto-reloading during development
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
 
